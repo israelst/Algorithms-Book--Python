be6d3f2224c97ef4f16139b3eacd41fe2c0d3d02
Israel Teixeira <israelst@gmail.com>
2012-05-10 04:16:38 -0300

    Add a solution without create a list in fib2

--- 0-Prologue/fib2.py
+++ 0-Prologue/fib2.py
@@ -7,6 +7,14 @@ def fib2(n):
         f[i] = f[i - 1] + f[i - 2]
     return f[n]
 
+# Create this list is useless since we just need the last calculated value
+def fib2_without_list(n):
+    if n == 0: return 0
+    f0, f1 = 0, 1
+    for i in range(2, n + 1):
+        f1, f0 = f0 + f1, f1
+    return f1
+
 assert fib2(0) == 0
 assert fib2(1) == 1
 assert fib2(2) == 1
@@ -14,3 +22,11 @@ assert fib2(3) == 2
 assert fib2(5) == 5
 assert fib2(6) == 8
 assert fib2(10) == 55
+
+assert fib2_without_list(0) == 0
+assert fib2_without_list(1) == 1
+assert fib2_without_list(2) == 1
+assert fib2_without_list(3) == 2
+assert fib2_without_list(5) == 5
+assert fib2_without_list(6) == 8
+assert fib2_without_list(10) == 55
